5bf733dc567e9a4c41a4a3e3721ef91cc29e803d
Jason Ky <744169@student.dusd.net>
2024-11-04 21:29:05 -0800

    Worked on Movement
    - Player can now smoothly move left to right
    
    - Player can now jump.

--- SkillsUSA25TeamSD/Assets/Scripts/PlayerMovement.cs
+++ SkillsUSA25TeamSD/Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float movementSpeed = 100;
+    [Header("Movement")]
+    public float movementSpeed = 50f;
+    public float jumpForce = 10f;
 
     Rigidbody playerRb;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    public bool grounded;
+    public float groundDrag;
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerRb.drag = groundDrag;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         PlayerMovements();
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 1f, whatIsGround);
+        if (grounded)
+        {
+            playerRb.drag = groundDrag;
+        }
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
     }
 
     private void PlayerMovements()
@@ -26,6 +45,13 @@ public class PlayerMovement : MonoBehaviour
         // Checks if the keys for the axis "Horizontal" are being inputted, and gives it a value between -1 and 1.
         float horizontal = Input.GetAxis("Horizontal");
 
-        playerRb.velocity = (Vector3.right * Time.deltaTime * movementSpeed * horizontal);
+        playerRb.AddForce(Vector3.right * movementSpeed * horizontal, ForceMode.Force);
+    }
+
+    private void Jump()
+    {
+        playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+        playerRb.drag /= 2;
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
