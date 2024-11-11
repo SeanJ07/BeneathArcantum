c7191dcac26eab618a87970848e74a721054c6dd
Hyunmin Park <753429@student.dusd.net>
2024-11-10 20:28:00 -0800

    added pause screen
    added pause screen
    changed game manager script
    added resume and main menu buttons

--- SkillsUSA25TeamSD/Assets/Scripts/GameManager.cs
+++ SkillsUSA25TeamSD/Assets/Scripts/GameManager.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public Camera mainCam;
+    public GameObject pauseScreen;
     GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         CameraFollow();
+        PauseGame();
     }
 
     void CameraFollow()
@@ -29,5 +34,25 @@ public class GameManager : MonoBehaviour
     void StartGame()
     {
 
+    }
+    
+    public void PauseGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    public void ResumeButton()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenuButton()
+    {
+
+        SceneManager.LoadScene("GameMenu");
     }
 }
