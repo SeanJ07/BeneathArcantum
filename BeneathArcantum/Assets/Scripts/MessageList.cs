8bba9c5d9b946eae49339b513bfedd3b5f5251a9
DerecDD <dereckdomidub@gmail.com>
2025-02-03 19:58:55 -0800

    Added the new map
    -Fixed script errors
    -Added colliders
    -Repositioned some elements to fix with new map
    -Fixed some bugs with lighting
    -Made level brighter

--- BeneathArcantum/Assets/Scripts/MessageList.cs
+++ BeneathArcantum/Assets/Scripts/MessageList.cs
@@ -23,7 +23,6 @@ public class MessageList : MonoBehaviour
         textOutput = GameObject.Find("TextOutput").GetComponent<TextMeshProUGUI>();
     }
 
-
     void Start() // Starts at the first message and initiates the typewriter effect for it.
     {
         currentMessage = 0;
@@ -37,52 +36,59 @@ public class MessageList : MonoBehaviour
         {
             StartCoroutine(NextMessage());
         }
+    }
 
-        if (currentMessage >= messages.Length) // Once all the messages are gone through, initiate end event ( turn off gameObject)
+    public IEnumerator NextMessage()
+    {
+        if (currentMessage < messages.Length - 1) // While the current message hasn't gone through all the messages, go to the next message after the current one is done.
+        {
+            currentMessage += 1;
+            StartCoroutine(typewriterEffect());
+            yield break;
+        }
+        else // Once all the messages are gone through, initiate end event (turn off gameObject)
         {
             currentMessage = 0;
             endEvent.Invoke();
         }
     }
 
-    public IEnumerator NextMessage()
-    {
-        if (currentMessage < messages.Length) // While the current message hasnt gone through all the messages, go to the next message after the current one is done.
-        {
-            currentMessage += 1;
-            StartCoroutine(typewriterEffect());
-
-            yield break;
-        }
-    }
 
     public void UpdateMessage()
     {
-        StartCoroutine(typewriterEffect());
+        if (currentMessage < messages.Length) // Ensure currentMessage is within bounds
+        {
+            StartCoroutine(typewriterEffect());
+        }
     }
 
     public void PreviousMessage() // Go back to the last message
     {
-        currentMessage -= 1;
-        textOutput.text = messages[currentMessage];
+        if (currentMessage > 0) // Ensure currentMessage does not go below 0
+        {
+            currentMessage -= 1;
+            textOutput.text = messages[currentMessage];
+        }
     }
 
     public IEnumerator typewriterEffect() // Does the typewriter effect
     {
         StopCoroutine(typewriterEffect());
-        string currentText = "";
-        currentLetter = 0;
 
-        textOutput.text = currentText;
-
-        for (currentLetter = 0; currentLetter < messages[currentMessage].Length + 1; currentLetter++)
+        if (currentMessage < messages.Length) // Ensure currentMessage is within bounds
         {
-            currentText = messages[currentMessage].Substring(0, currentLetter);
+            string currentText = "";
+            currentLetter = 0;
+
             textOutput.text = currentText;
 
-            yield return new WaitForSeconds(textDelay);
+            for (currentLetter = 0; currentLetter < messages[currentMessage].Length + 1; currentLetter++)
+            {
+                currentText = messages[currentMessage].Substring(0, currentLetter);
+                textOutput.text = currentText;
+
+                yield return new WaitForSeconds(textDelay);
+            }
         }
-
-
     }
 }
